7273fff2487be5ebb5cebb48797ead7f80dac0ab
hyuk0309 <bahngeh@naver.com>
2023-07-12 18:39:43 +0900

    test : edit fail test

--- src/test/kotlin/elvis/chat/playgroundchatserver/PlaygroundChatServerApplicationTests.kt
+++ src/test/kotlin/elvis/chat/playgroundchatserver/PlaygroundChatServerApplicationTests.kt
@@ -2,12 +2,15 @@ package elvis.chat.playgroundchatserver
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.web.WebAppConfiguration
 
 @SpringBootTest
+@Import(ServletWebServerApplicationContext::class)
 class PlaygroundChatServerApplicationTests {
-
     @Test
     fun contextLoads() {
     }
-
 }
